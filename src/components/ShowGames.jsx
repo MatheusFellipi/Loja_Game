2122835510ee427550b0e292c3d88a3e09480b20
Mathues Fellipi <47674343+MatheusFellipi@users.noreply.github.com>
2021-03-06 19:38:02 -0300

    arrumando os calculos

--- src/components/ShowGames.jsx
+++ src/components/ShowGames.jsx
@@ -1,31 +1,25 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { purchasePrice  } from '../context/PurchasePrice';
 import dadosGames from "../products.json"
 
 
 import sc from "../assets/fifa-18.png"
 import {CardGame,Container,Span,Title} from "../styles/components/stylesShowGames"
-function ShowGames() {
-  const {setPrice,calculateFreight,setProduct,setPriceTotal} = useContext(purchasePrice);
-  
-  const [priceTotalCompo,setPriceTotalCompo] = useState();
 
+function ShowGames() {
+
+  const {setProduct,calculatePrice} = useContext(purchasePrice);
+
+  
   const purchaseItem =(priceItem,gameId,gameName)=>{
-   const total = priceTotalCompo + priceItem
-   
-  console.log(priceTotalCompo);
    const product = {
     gameId:gameId, 
     name:gameName,
     price:priceItem
-   }
-
-   setPriceTotalCompo(total)
-   setPriceTotal(total);
-   setPrice(total);
+   }   
    setProduct((oldArr)=>[...oldArr,product])
+   calculatePrice();
 
-   calculateFreight();
   }
 
   return (
--- src/context/PurchasePrice.jsx
+++ src/context/PurchasePrice.jsx
@@ -3,22 +3,29 @@ import { createContext, useState } from "react";
 export const purchasePrice = createContext({});
 
 export function PurchasePriceProvider({ children }) {
+  const [product, setProduct] = useState([]);
+  
   const [price, setPrice] = useState(0);
   const [priceTotal, setPriceTotal] = useState(0);
+  
   const [freight, setFreight] = useState(10);
-  const [product, setProduct] = useState([]);
 
-  console.log(product);
   const toRemove = (gameID,priceItem) => {
     product.splice(product.indexOf(gameID));
-    
     setPrice(price - priceItem);
-    
     calculateFreight();
-
-    console.log(priceTotal);
-    console.log(product);
   };
+  console.log(product);
+
+  function calculatePrice() {
+    const total = product.reduce(getTotal,0)
+    console.log(total);
+  }
+
+  function getTotal(total, item) {
+    return total + item.price;
+   }
+
 
   const calculateFreight = () => {
     if (price >= 250) {
@@ -36,6 +43,7 @@ export function PurchasePriceProvider({ children }) {
         priceTotal,
         freight,
         calculateFreight,
+        calculatePrice,
         setProduct,
         setPriceTotal,
         toRemove,
